b63da40175a357d3be51834b2daf4b0a3cb99825
Official-MahnoorTariq <real.mahnoor.tariq@gmail.com>
2026-04-01 01:37:02 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import One from "./components/One";
+
+export default function App(){
+  return(
+    <>
+    <One/>
+    </>
+  );
+}
---
+++ src/components/One.jsx
@@ -0,0 +1,183 @@
+export default function One() {
+  return (
+    <>
+    <div className="outer">
+      {}
+      <div className="box_row">
+        <div className="box" style={{ border: "50px solid rgb(147, 1, 1)" }}>
+          <div className="circle border_red"></div>
+          <div className="circle border_red"></div>
+          <div className="circle border_red"></div>
+          <div className="circle border_red"></div>
+        </div>
+
+        <div className="v_lad">
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell green"></div>
+            <div className="v_lad_cell green">
+              <span className="star">&#9733;</span>
+            </div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell green">
+              <span className="star">&#9733;</span>
+            </div>
+            <div className="v_lad_cell green"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell green"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell green"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell green"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+        </div>
+
+        <div className="box" style={{ border: "50px solid rgb(2, 118, 2)" }}>
+          <div className="circle border_green"></div>
+          <div className="circle border_green"></div>
+          <div className="circle border_green"></div>
+          <div className="circle border_green"></div>
+        </div>
+      </div>
+
+      {}
+      <div className="middle_row">
+        <div className="h_lad">
+          <div className="h_lad_row">
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell red">
+              <span className="star">&#9733;</span>
+            </div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+          </div>
+          <div className="h_lad_row">
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell red"></div>
+            <div className="h_lad_cell red"></div>
+            <div className="h_lad_cell red"></div>
+            <div className="h_lad_cell red"></div>
+            <div className="h_lad_cell red"></div>
+          </div>
+          <div className="h_lad_row">
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell red">
+              <span className="star">&#9733;</span>
+            </div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+          </div>
+        </div>
+
+        <div className="ludo_home"></div>
+
+        <div className="h_lad">
+          <div className="h_lad_row">
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell yellow">
+              <span className="star">&#9733;</span>
+            </div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+          </div>
+          <div className="h_lad_row">
+            <div className="h_lad_cell yellow"></div>
+            <div className="h_lad_cell yellow"></div>
+            <div className="h_lad_cell yellow"></div>
+            <div className="h_lad_cell yellow"></div>
+            <div className="h_lad_cell yellow"></div>
+            <div className="h_lad_cell"></div>
+          </div>
+          <div className="h_lad_row">
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell"></div>
+            <div className="h_lad_cell yellow">
+              <span className="star">&#9733;</span>
+            </div>
+            <div className="h_lad_cell"></div>
+          </div>
+        </div>
+      </div>
+      {}
+      <div className="box_row">
+        <div className="box" style={{ border: "50px solid rgb(2, 2, 84)" }}>
+          <div className="circle border_blue"></div>
+          <div className="circle border_blue"></div>
+          <div className="circle border_blue"></div>
+          <div className="circle border_blue"></div>
+        </div>
+
+        <div className="v_lad">
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell blue"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell blue"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell blue"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell blue"></div>
+            <div className="v_lad_cell blue">
+              <span className="star">&#9733;</span>
+            </div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell blue">
+              <span className="star">&#9733;</span>
+            </div>
+            <div className="v_lad_cell blue"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+          <div className="v_lad_row">
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell"></div>
+            <div className="v_lad_cell"></div>
+          </div>
+        </div>
+
+        <div className="box" style={{ border: "50px solid rgb(197, 197, 5)" }}>
+          <div className="circle border_yellow"></div>
+          <div className="circle border_yellow"></div>
+          <div className="circle border_yellow"></div>
+          <div className="circle border_yellow"></div>
+        </div>
+      </div>
+    </div>
+  
+ 
+    </>
+  );
+}
